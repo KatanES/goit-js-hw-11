5671b28ba315ac21f5468b4da4fedf6890f31170
KatanES <businesskatan.es@gmail.com>
2023-07-09 18:40:13 +0200

    axios code

--- src/index.js
+++ src/index.js
@@ -18,39 +18,41 @@ let totalHits = 0;
 form.addEventListener('submit', handleSubmit);
 loadBtn.addEventListener('click', handleLoadMore);
 
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
   page = 1; // Reset the page to 1 for a new search
   const value = input.value;
-  fetchImages(value);
+  await fetchImages(value);
 }
 
-function handleLoadMore() {
+async function handleLoadMore() {
   const value = input.value;
-  fetchImages(value);
+  await fetchImages(value);
 }
 
-function fetchImages(value) {
+async function fetchImages(value) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      if (data.hits.length === 0) {
-        showError();
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    if (data.hits.length === 0) {
+      showError();
+    } else {
+      showImages(data.hits);
+      totalHits = data.totalHits;
+      if (page * perPage < totalHits) {
+        loadBtn.hidden = false;
       } else {
-        showImages(data.hits);
-        totalHits = data.totalHits;
-        if (page * perPage < totalHits) {
-          loadBtn.hidden = false;
-        } else {
-          loadBtn.hidden = true;
-          showEndMessage();
-        }
+        loadBtn.hidden = true;
+        showEndMessage();
       }
-      page++;
-    })
-    .catch(showError);
+    }
+    page++;
+  } catch (error) {
+    showError();
+  }
 }
 
 function showImages(images) {
@@ -89,3 +91,97 @@ function showEndMessage() {
     "We're sorry, but you've reached the end of search results."
   );
 }
+
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '38005308-94b85d06f84497fefd0aa075c';
+
+// import axios from 'axios';
+// import Notiflix from 'notiflix';
+
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+// const form = document.getElementById('search-form');
+// const input = document.querySelector('.js-input');
+// const gallery = document.querySelector('.js-gallery');
+// const loadBtn = document.querySelector('.load-js');
+
+// let page = 1;
+// let perPage = 40;
+// let totalHits = 0;
+
+// form.addEventListener('submit', handleSubmit);
+// loadBtn.addEventListener('click', handleLoadMore);
+
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   page = 1; // Reset the page to 1 for a new search
+//   const value = input.value;
+//   await fetchImages(value);
+// }
+
+// async function handleLoadMore() {
+//   const value = input.value;
+//   await fetchImages(value);
+// }
+
+// async function fetchImages(value) {
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     if (data.hits.length === 0) {
+//       showError();
+//     } else {
+//       showImages(data.hits);
+//       totalHits = data.totalHits;
+//       if (page * perPage < totalHits) {
+//         loadBtn.hidden = false;
+//       } else {
+//         loadBtn.hidden = true;
+//         showEndMessage();
+//       }
+//     }
+//     page++;
+//   } catch (error) {
+//     showError();
+//   }
+// }
+
+// function showImages(images) {
+//   images.forEach(image => {
+//     const { tags, webformatURL, likes, views, comments, downloads } = image;
+//     const markup = `
+//       <div class="photo-card">
+//         <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/>
+//         <div class="info">
+//           <p class="info-item">
+//             <b>Likes ${likes}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>Views ${views}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>Comments ${comments}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>Downloads ${downloads}</b>
+//           </p>
+//         </div>
+//       </div>`;
+//     gallery.insertAdjacentHTML('beforeend', markup);
+//   });
+// }
+
+// function showError() {
+//   Notiflix.Notify.failure(
+//     'Sorry, there are no images matching your search query. Please try again.'
+//   );
+// }
+
+// function showEndMessage() {
+//   Notiflix.Notify.info(
+//     "We're sorry, but you've reached the end of search results."
+//   );
+// }
