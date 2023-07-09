e04e9ee08fe9e63b9b8b3f266f9c035bd147ad4b
KatanES <businesskatan.es@gmail.com>
2023-07-09 19:31:08 +0200

    Reset the page to 1 for a new search

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const API_KEY = '38005308-94b85d06f84497fefd0aa075c';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-axios.defaults.headers.common['x-api-key'] = API_KEY;
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 const form = document.getElementById('search-form');
 const input = document.querySelector('.js-input');
@@ -14,6 +14,7 @@ const loadBtn = document.querySelector('.load-js');
 let page = 1;
 let perPage = 40;
 let totalHits = 0;
+let searchQuery = '';
 
 form.addEventListener('submit', handleSubmit);
 loadBtn.addEventListener('click', handleLoadMore);
@@ -21,6 +22,7 @@ loadBtn.addEventListener('click', handleLoadMore);
 async function handleSubmit(e) {
   e.preventDefault();
   page = 1; // Reset the page to 1 for a new search
+  gallery.innerHTML = ''; // Clear the existing gallery
   const value = input.value;
   await fetchImages(value);
 }
@@ -35,7 +37,7 @@ async function fetchImages(value) {
 
   try {
     const response = await axios.get(url);
-    const data = response.data;
+    const data = await response.data;
 
     if (data.hits.length === 0) {
       showError();
@@ -49,7 +51,7 @@ async function fetchImages(value) {
         showEndMessage();
       }
     }
-    page++;
+    page += 1;
   } catch (error) {
     showError();
   }
@@ -92,96 +94,7 @@ function showEndMessage() {
   );
 }
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '38005308-94b85d06f84497fefd0aa075c';
-
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
-
-// axios.defaults.headers.common['x-api-key'] = API_KEY;
-
-// const form = document.getElementById('search-form');
-// const input = document.querySelector('.js-input');
-// const gallery = document.querySelector('.js-gallery');
-// const loadBtn = document.querySelector('.load-js');
-
-// let page = 1;
-// let perPage = 40;
-// let totalHits = 0;
-
-// form.addEventListener('submit', handleSubmit);
-// loadBtn.addEventListener('click', handleLoadMore);
-
-// async function handleSubmit(e) {
-//   e.preventDefault();
-//   page = 1; // Reset the page to 1 for a new search
-//   const value = input.value;
-//   await fetchImages(value);
-// }
-
-// async function handleLoadMore() {
-//   const value = input.value;
-//   await fetchImages(value);
-// }
-
-// async function fetchImages(value) {
-//   const url = `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-
-//     if (data.hits.length === 0) {
-//       showError();
-//     } else {
-//       showImages(data.hits);
-//       totalHits = data.totalHits;
-//       if (page * perPage < totalHits) {
-//         loadBtn.hidden = false;
-//       } else {
-//         loadBtn.hidden = true;
-//         showEndMessage();
-//       }
-//     }
-//     page++;
-//   } catch (error) {
-//     showError();
-//   }
-// }
-
-// function showImages(images) {
-//   images.forEach(image => {
-//     const { tags, webformatURL, likes, views, comments, downloads } = image;
-//     const markup = `
-//       <div class="photo-card">
-//         <img src="${webformatURL}" alt="${tags}" loading="lazy" class="img"/>
-//         <div class="info">
-//           <p class="info-item">
-//             <b>Likes ${likes}</b>
-//           </p>
-//           <p class="info-item">
-//             <b>Views ${views}</b>
-//           </p>
-//           <p class="info-item">
-//             <b>Comments ${comments}</b>
-//           </p>
-//           <p class="info-item">
-//             <b>Downloads ${downloads}</b>
-//           </p>
-//         </div>
-//       </div>`;
-//     gallery.insertAdjacentHTML('beforeend', markup);
-//   });
-// }
-
-// function showError() {
-//   Notiflix.Notify.failure(
-//     'Sorry, there are no images matching your search query. Please try again.'
-//   );
-// }
-
-// function showEndMessage() {
-//   Notiflix.Notify.info(
-//     "We're sorry, but you've reached the end of search results."
-//   );
+// Функція для оновлення пошукового запиту
+// function setSearchQuery(newQuery) {
+//   searchQuery = newQuery;
 // }
