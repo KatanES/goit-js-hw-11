80d9f0128b0077b81c54f1ffc6b72c529acf2b79
KatanES <businesskatan.es@gmail.com>
2023-07-10 21:25:12 +0200

    FIX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38005308-94b85d06f84497fefd0aa075c';
 
-import axios from 'axios';
+// import axios from 'axios';
 import Notiflix from 'notiflix';
 
 // axios.defaults.headers.common['x-api-key'] = API_KEY;
